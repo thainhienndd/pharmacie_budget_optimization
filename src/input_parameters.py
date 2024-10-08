664699fc5f1e114b7bcb5cd427ecf097b9d09b14
anxuan2 <tnnddnguyendinhdo@Air-de-TNNDD.lan>
2024-10-08 23:11:18 +0200

    new test for a path

--- src/input_parameters.py
+++ src/input_parameters.py
@@ -3,8 +3,10 @@ is_local = False
 if is_local:
     prefix_path = '../data/'
 else:
-    prefix_path = 'thainhienndd/master/pharmacie_budget_optimization/data/'
+    prefix_path = 'thainhienndd/pharmacie_budget_optimization/master/data/'
 
 factures_path = prefix_path + 'sorties/factures.xlsx'
 salaires_path = prefix_path + 'sorties/salaires.xlsx'
 prelevements_path = prefix_path + 'sorties/prelevements.xlsx'
+
+
