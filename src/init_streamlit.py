7db75c2dcf90e7a51822611b5e0c7121f99fbc7d
anxuan2 <tnnddnguyendinhdo@Air-de-TNNDD.lan>
2024-10-08 23:01:51 +0200

    add prefix path

--- src/init_streamlit.py
+++ src/init_streamlit.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+from input_parameters import *
 
 
 def initialize_streamlit():
     if 'factures' not in st.session_state:
-        st.session_state.factures = pd.read_excel('../data/sorties/factures.xlsx')
+        st.session_state.factures = pd.read_excel(factures_path)
     if 'salaires' not in st.session_state:
-        st.session_state.salaires = pd.read_excel('../data/sorties/salaires.xlsx')
+        st.session_state.salaires = pd.read_excel(salaires_path)
     if 'prelevements' not in st.session_state:
-        st.session_state.prelevements = pd.read_excel('../data/sorties/prelevements.xlsx')
+        st.session_state.prelevements = pd.read_excel(prelevements_path)
---
+++ src/input_parameters.py
@@ -0,0 +1,10 @@
+is_local = False
+
+if is_local:
+    prefix_path = '../data/'
+else:
+    prefix_path = 'pharmacie_budget_optimization/data/'
+
+factures_path = prefix_path + 'sorties/factures.xlsx'
+salaires_path = prefix_path + 'sorties/salaires.xlsx'
+prelevements_path = prefix_path + 'sorties/prelevements.xlsx'
